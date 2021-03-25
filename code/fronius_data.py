f742d1e579020aed8cb4cd0fe45df851f65d916e
David <rayder_1_3@hotmail.com>
2021-03-25 21:34:54 +1000

    update

--- code/fronius_data.py
+++ code/fronius_data.py
@@ -6,9 +6,10 @@
 
 import json
 import requests
+import time
 
-#Set debug on/off
-isDebug = True
+#Set debug on/off ---- maybe set to an arg for release debugging?
+isDebug = False
 
 # Fronius API 
 # Inverter IP/Host address  ---- do i need to use meter endpoint or can i calculate everything i need from powerflow endpoint?
@@ -17,14 +18,17 @@ inverterMtrEndPt = "/solar_api/v1/GetMeterRealtimeData.cgi?Scope=Device&DeviceId
 inverterPfEndPt = "/solar_api/v1/GetPowerFlowRealtimeData.fcgi"
 
 # Options
-requestInterval = 60 # 60 seconds
+requestInterval = 10 # 60 seconds
 
+energyUsedHrAvg = 0
+energyUsedHrRunning = 0
+energyUsedHrCount = 0
 
 # Do all the things
 
 # Get Fronius JSON data and loads it into jsonData for use
-jsonResponse = requests.get("http://" + inverterAddr + inverterPfEndPt)
-jsonPfData = json.loads(jsonResponse.text)
+#jsonResponse = requests.get("http://" + inverterAddr + inverterPfEndPt)
+#jsonPfData = json.loads(jsonResponse.text)
 jsonResponse = requests.get("http://" + inverterAddr + inverterMtrEndPt)
 jsonMtrData = json.loads(jsonResponse.text)
 
@@ -32,3 +36,43 @@ jsonMtrData = json.loads(jsonResponse.text)
 if isDebug :
     print(jsonPfData['Body']['Data']['Site']['P_Load'])
     print(jsonPfData['Body']['Data']['Site']['P_Grid'])
+
+while True:
+    jsonResponse = requests.get("http://" + inverterAddr + inverterPfEndPt)
+    jsonPfData = json.loads(jsonResponse.text)
+
+    froniusPLoad = jsonPfData['Body']['Data']['Site']['P_Load']
+    froniusPGrid = jsonPfData['Body']['Data']['Site']['P_Grid']
+    #global energyUsedHrAvg
+    #global energyUsedHrRunning
+    #global energyUsedHrCount
+
+    if jsonPfData['Body']['Data']['Site']['P_Load'] >= 0 :
+        print('Feeding in ' + str(froniusPLoad) + 'w to the grid')
+        if energyUsedHrCount <= 600 :
+            energyUsedHrCount += 1
+            energyUsedHrRunning += int(froniusPLoad)
+            energyUsedHrAvg = energyUsedHrRunning / energyUsedHrCount
+        else :
+            energyUsedHrCount = 1
+            energyUsedHrRunning = 0
+            energyUsedHrAvg = 0
+            
+        print('kWh average for the current hour: ' + str(energyUsedHrAvg))
+
+        time.sleep(requestInterval)
+
+    elif jsonPfData['Body']['Data']['Site']['P_Load'] < 0 :
+        print('Drawing ' + str(froniusPLoad * -1) + 'w from the grid')
+        if energyUsedHrCount <= 600 :
+            energyUsedHrCount += 1
+            energyUsedHrRunning += int(froniusPLoad * -1)
+            energyUsedHrAvg += energyUsedHrRunning / ((energyUsedHrCount * requestInterval) * 60)
+        else :
+            energyUsedHrCount = 1
+            energyUsedHrRunning = 0
+            energyUsedHrAvg = 0
+
+        print('kWh average for the current hour: ' + str(energyUsedHrAvg))
+
+        time.sleep(requestInterval)
