47b87fb4d62f6a1c94cfa1fdd5916b466d6a4357
David <rayder_1_3@hotmail.com>
2021-03-25 16:38:35 +1000

    update

--- code/main.py
+++ code/main.py
@@ -1,21 +1,25 @@
 import json
 import requests
 
-# Fronius inverter IP/Host address
+#Set debug on/off
+isDebug = True
+
+# Fronius API 
+# Inverter IP/Host address
 inverterAddr = "192.168.20.5"
-# Selected Fronius JSON endpoint
+# JSON endpoint
 inverterEndPt = "/solar_api/v1/GetMeterRealtimeData.cgi?Scope=Device&DeviceId=0"
+
+
 
 # Get Fronius JSON data and loads it into jsonData for use
 jsonResponse = requests.get("http://" + inverterAddr + inverterEndPt)
 jsonData = json.loads(jsonResponse.text)
 
-# Print out JSON data for debug  !!!Only for debugging!!!
-print(jsonData)
+# Print out JSON data for debug
+if isDebug : print(jsonData)
 
 # Store Fronius JSON data for calculations
-
-
 
 # **Hourly/daily/weekly/monthly production data
 
